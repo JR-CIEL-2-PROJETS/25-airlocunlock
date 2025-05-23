0c4543f02c807739b6258ba569fb32fec81af6d9
bilalTaoufik0 <bilal.taoufik@lycee-jeanrostand.fr>
2025-05-23 19:12:12 +0200

    save

--- docker-down.sh
+++ docker-down.sh
@@ -4,12 +4,12 @@ echo "💾 Sauvegarde des bases de données dans le dossier code/..."
 docker exec -i mysql-container mysqldump -u root -proot airlockunlock > code/back_airlockunlock.sql
 docker exec -i mysql-container mysqldump -u root -proot Tapkey > code/back_tapkey.sql
 
-echo "📦 Ajout, commit et push Git des fichiers SQL dans code/..."
+echo "📦 Ajout, commit et push Git des fichiers SQL"
 git add .
 git commit -m "save"
 git push origin API-1
 
 echo "🧹 Arrêt des containers..."
-docker compose down
+docker compose-down
 
 echo "✅ Bases sauvegardées dans, commit Git fait, containers arrêtés."
--- docker-up.sh
+++ docker-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "📦 Lancement des containers..."
-docker compose up -d
+docker-compose up -d
 
 echo "⏳ Attente du démarrage de MySQL..."
 sleep 10  # tu peux ajuster le délai ou utiliser un healthcheck plus avancé
