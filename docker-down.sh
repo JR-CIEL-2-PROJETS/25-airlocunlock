a7e2391217f568e25819f816a9d9fc45e03a952d
bilalTaoufik0 <bilal.taoufik@lycee-jeanrostand.fr>
2025-05-24 17:34:11 +0200

    sauvegarde des bases

--- docker-down.sh
+++ docker-down.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "💾 Sauvegarde des bases de données dans APIs/code/ ..."
-docker exec mysql_container mysqldump -u root -pROOT_PASSWORD back_airlockunlock > APIs/code/back_airlockunlock.sql
-docker exec mysql_container mysqldump -u root -pROOT_PASSWORD back_tapkey > APIs/code/back_tapkey.sql
+docker exec mysql_container mysqldump -u root -proot airlockunlock > APIs/code/back_airlockunlock.sql
+docker exec mysql_container mysqldump -u root -proot Tapkey > APIs/code/back_tapkey.sql
 
 echo "⬆️ Push Git"
 git add .
--- docker-up.sh
+++ docker-up.sh
@@ -7,7 +7,7 @@ echo "⏳ Attente du démarrage de MySQL..."
 sleep 10
 
 echo "📦 Importation des bases de données..."
-docker exec mysql_container mysqldump -u root -pROOT_PASSWORD back_airlockunlock < APIs/code/back_airlockunlock.sql
-docker exec mysql_container mysqldump -u root -pROOT_PASSWORD back_tapkey < APIs/code/back_tapkey.sql
+docker exec mysql_container mysqldump -u root -proot airlockunlock < APIs/code/back_airlockunlock.sql
+docker exec mysql_container mysqldump -u root -proot Tapkey < APIs/code/back_tapkey.sql
 
 echo "✅ Conteneurs lancés et bases importées."
