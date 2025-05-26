c9686d4db3d59fc4877d14d7b23429f607679af7
ElmirAmine <amine.elmir@lycee-jeanrostand.fr>
2025-05-26 11:07:03 +0200

    save

--- docker-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "📦 Lancement des containers..."
-docker-compose up -d
-
-echo "⏳ Attente du démarrage de MySQL..."
-sleep 10  # tu peux ajuster le délai ou utiliser un healthcheck plus avancé
-
