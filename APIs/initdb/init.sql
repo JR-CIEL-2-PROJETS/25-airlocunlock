de2f5a603cb90ea89323bd4a90d4325617cf12ee
bilalTaoufik0 <bilal.taoufik@lycee-jeanrostand.fr>
2025-06-02 01:48:38 +0200

    save

--- APIs/initdb/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
-INSERT INTO users (username, email, password) VALUES
-('admin', 'admin@airlockunlock.com', 'admin123');
