4f2267b14093c276bb862f5de936389a963352c8
bilalTaoufik0 <bilal.taoufik@lycee-jeanrostand.fr>
2025-06-03 13:33:12 +0200

    save

--- APIs/code/AirlockUnlock/config.php
+++ APIs/code/AirlockUnlock/config.php
@@ -3,16 +3,16 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-// Configuration de la base de données
 $host = 'mysql'; 
 $dbname = 'airlockunlock'; 
 $username = 'root'; 
 $password = 'root';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Échec de la connexion à la base de données : " . $e->getMessage();
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['message' => "Échec de la connexion à la base de données : " . $e->getMessage()]);
+    exit();
 }
-?>
