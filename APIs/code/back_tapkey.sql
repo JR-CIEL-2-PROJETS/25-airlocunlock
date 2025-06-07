e0a1c095113efa056f86bfdcc2f28602f2bc067b
Amine El mir <amine.elmir@lycee-jeanrostand.fr>
2025-06-08 01:39:07 +0200

    🚀 Backup et arrêt des services via docker-down.sh

--- APIs/code/back_tapkey.sql
+++ APIs/code/back_tapkey.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.4.5, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.4.4, for Linux (x86_64)
 --
 -- Host: localhost    Database: Tapkey
 -- ------------------------------------------------------
--- Server version	8.4.5
+-- Server version	8.4.4
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-05 23:32:52
+-- Dump completed on 2025-06-07 23:38:58
