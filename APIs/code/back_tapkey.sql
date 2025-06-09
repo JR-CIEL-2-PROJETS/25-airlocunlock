40d8201ddffa9467338c378c17fef65271a52dc9
AdelAichi <adel.aichi@lycee-jeanrostand.fr>
2025-06-09 23:46:05 +0200

    🚀 Backup et arrêt des services via docker-down.sh

--- APIs/code/back_tapkey.sql
+++ APIs/code/back_tapkey.sql
@@ -14,6 +14,34 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cles_electroniques`
+--
+
+DROP TABLE IF EXISTS `cles_electroniques`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cles_electroniques` (
+  `id_cle` int NOT NULL AUTO_INCREMENT,
+  `numero_serie` varchar(100) NOT NULL,
+  `proprietaire_nom` varchar(100) DEFAULT NULL,
+  `date_activation` datetime DEFAULT CURRENT_TIMESTAMP,
+  `statut` enum('active','inactive') DEFAULT 'active',
+  PRIMARY KEY (`id_cle`),
+  UNIQUE KEY `numero_serie` (`numero_serie`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cles_electroniques`
+--
+
+LOCK TABLES `cles_electroniques` WRITE;
+/*!40000 ALTER TABLE `cles_electroniques` DISABLE KEYS */;
+INSERT INTO `cles_electroniques` VALUES (1,'TK-001-XYZ','Lakshan Sangaralingam','2025-04-08 09:38:37','active'),(2,'TK-002-XYZ','Lakshan Sangaralingam','2025-05-04 12:22:40','active'),(3,'TK-003-XYZ','Lakshan Sangaralingam','2025-05-05 12:23:22','active'),(4,'TK-004-XYZ','Lakshan Sangaralingam','2025-06-03 11:28:19','active');
+/*!40000 ALTER TABLE `cles_electroniques` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +52,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-09 21:36:58
+-- Dump completed on 2025-06-09 21:45:58
